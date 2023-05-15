478a1bd4f63ce22418cc8aa35c80aae222f903f4
walmslea <98186467+walmslea@users.noreply.github.com>
2023-05-14 23:33:06 -0500

    Completed Project 2

---
+++ blog/gatsby-config.js
@@ -0,0 +1,62 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
+ */
+
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`
+})
+
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Blog`,
+    description: `ITDEV-164 Gatsby Blog.`,
+    author: `Patrick Walmsley`,
+    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    contact: {
+      name: `Patrick Walmsley`,
+      company: `Blogs Inc.`,
+      address: `PO Box 6969`
+    }
+  },
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `${process.env.SPACE_ID}`,
+        accessToken: `${process.env.ACCESS_TOKEN}`
+      }
+    },
+    `gatsby-transformer-remark`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+  ],
+}
+
